e49163b72b8708d870a92fb11e0f66e87dffbb69
nintichance <mintidance@gmail.com>
2018-01-07 16:18:37 -0500

    Solved connection routes bug

--- routes/connectionsController.js
+++ routes/connectionsController.js
@@ -1,67 +1,70 @@
-var express = require('express');
-var router = express.Router({mergeParams: true});
+const express = require('express')
+const router = express.Router({ mergeParams: true })
 
 const User = require('../db/models/User')
+
 
 router.get('/new', (req, res) => {
     const userId = req.params.userId
     const effectId = req.params.effectId
-  
+
     User.findById(userId)
-      .then((user) => {
-        const effect = user.effects.id(effectId)
-  
-        res.render('connections/new', {
-          userId,
-          effect,
-          effectId: user.effects.id(effectId),
-          pageTitle: 'new connection'
+        .then((user) => {
+            const effect = user.effects.id(effectId)
+
+            res.render('connections/new', {
+                userId,
+                effect,
+                title: 'connections'
+            })
         })
-      })
-      .catch((err)=>{
-          console.log(err)
-      })
-  })
-  
-  router.post('/', (req, res) => {
+})
+
+router.post('/', (req, res) => {
     const userId = req.params.userId
     const effectId = req.params.effectId
-  
-    const newEffect = req.body
-  
-    User.findById(userId)
-      .then((user) => {
-        const effect = user.effects.id(effectId)
-        effect.connections.push(newEffect)
-  
-        return user.save()
-      })
-      .then(() => {
-        res.redirect(`/users/${userId}/effects/${effectId}`)
-      })
-  })
-  
-  router.get('/', (request, response) => {
-    const userId = request.params.userId
-    const effectId = request.params.effectId
-    const connectionId = request.params.connectionId
-  
-    User.findById(userId)
-      .then((user) => {
-        const effect = user.effects.id(effectId)
-        const connection = effect.connections.id(connectionId)
-  
-        response.render('connections/show', {
-          userId,
-          username: user.username,
-          effect,
-          connection,
-          pageTitle: 'connections'
-        })
-      })
-      .catch((error) => {
-        console.log(error)
-      })
-  })
 
-module.exports = router;
+    const newConnection = req.body
+    console.log(req.body)
+    User.findById(userId)
+        .then((user) => {
+            const effect = user.effects.id(effectId)
+            effect.connections.push(newConnection)
+
+            return user.save()
+        })
+        .then(() => {
+            res.redirect(`/users/${userId}/effects/${effectId}`)
+        })
+})
+
+router.get('/:connectionId', (req, res) => {
+    const userId = req.params.userId
+    const effectId = req.params.effectId
+    const connectionId = req.params.connectionId
+
+    User.findById(userId)
+        .then((user) => {
+            const effect = user.effects.id(effectId)
+            const connection = effect.connections.id(connectionId)
+            console.log(connection)
+            res.render('connections/show', {
+                userId,
+                effect,
+                username: user.username,
+                connection,
+                title: 'connections'
+            })
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+})
+
+
+
+
+
+
+
+module.exports = router
